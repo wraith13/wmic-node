f7ec3a66d6b46ac0566bd96a94949f469ad632c7
道化師 <wraith13@hotmail.co.jp>
2019-06-07 22:35:41 +0900

    🛠 実装( WIP )

--- source/index.ts
+++ source/index.ts
@@ -1,4 +1,409 @@
 export module wmic
 {
+    export module alias
+    {
+        
+    }
+
+    export module baseboard
+    {
+        
+    }
+    
+    export module bios
+    {
+        
+    }
+
+    export module bootconfig
+    {
+        
+    }
+
+    export module cdrom
+    {
+        
+    }
+
+    export module computersystem
+    {
+        
+    }
+
+    export module cpu
+    {
+        
+    }
+
+    export module csproduct
+    {
+        
+    }
+
+    export module datafile
+    {
+        
+    }
+
+    export module dcomapp
+    {
+        
+    }
+
+    export module desktop
+    {
+        
+    }
+
+    export module desktopmonitor
+    {
+        
+    }
+
+    export module devicememoryaddress
+    {
+        
+    }
+
+    export module diskdrive
+    {
+        
+    }
+
+    export module diskquota
+    {
+        
+    }
+
+    export module dmachannel
+    {
+        
+    }
+
+    export module environment
+    {
+        
+    }
+
+    export module fsdir
+    {
+        
+    }
+
+    export module group
+    {
+        
+    }
+
+    export module idecontroller
+    {
+        
+    }
+
+    export module irq
+    {
+        
+    }
+
+    export module job
+    {
+        
+    }
+
+    export module loadorder
+    {
+        
+    }
+
+    export module logicaldisk
+    {
+        
+    }
+
+    export module logon
+    {
+        
+    }
+
+    export module memcache
+    {
+        
+    }
+
+    export module memorychip
+    {
+        
+    }
+
+    export module memphysical
+    {
+        
+    }
+
+    export module netclient
+    {
+        
+    }
+
+    export module netlogin
+    {
+        
+    }
+
+    export module netprotocol
+    {
+        
+    }
+
+    export module netuse
+    {
+        
+    }
+
+    export module nic
+    {
+        
+    }
+
+    export module nicconfig
+    {
+        
+    }
+
+    export module ntdomain
+    {
+        
+    }
+
+    export module ntevent
+    {
+        
+    }
+
+    export module nteventlog
+    {
+        
+    }
+
+    export module onboarddevice
+    {
+        
+    }
+
+    export module os
+    {
+        
+    }
+
+    export module pagefile
+    {
+        
+    }
+
+    export module pagefileset
+    {
+        
+    }
+
+    export module partition
+    {
+        
+    }
+
+    export module port
+    {
+        
+    }
+
+    export module portconnector
+    {
+        
+    }
+
+    export module printer
+    {
+        
+    }
+
+    export module printerconfig
+    {
+        
+    }
+
+    export module printjob
+    {
+        
+    }
+
+    export module process
+    {
+        
+    }
+
+    export module product
+    {
+        
+    }
+
+    export module qfe
+    {
+        
+    }
+
+    export module quotasetting
+    {
+        
+    }
+
+    export module rdaccount
+    {
+        
+    }
+
+    export module rdnic
+    {
+        
+    }
+
+    export module rdpermissions
+    {
+        
+    }
+
+    export module rdtoggle
+    {
+        
+    }
+
+    export module recoveros
+    {
+        
+    }
+
+    export module registry
+    {
+        
+    }
+
+    export module scsicontroller
+    {
+        
+    }
+
+    export module server
+    {
+        
+    }
+
+    export module service
+    {
+        
+    }
+
+    export module shadowcopy
+    {
+        
+    }
+
+    export module shadowstorage
+    {
+        
+    }
+
+    export module share
+    {
+        
+    }
+
+    export module softwareelement
+    {
+        
+    }
+
+    export module softwarefeature
+    {
+        
+    }
+
+    export module sounddev
+    {
+        
+    }
+
+    export module startup
+    {
+        
+    }
+
+    export module sysaccount
+    {
+        
+    }
+
+    export module sysdriver
+    {
+        
+    }
+
+    export module systemenclosure
+    {
+        
+    }
+
+    export module systemslot
+    {
+        
+    }
+
+    export module tapedrive
+    {
+        
+    }
+
+    export module temperature
+    {
+        
+    }
+
+    export module timezone
+    {
+        
+    }
+
+    export module ups
+    {
+        
+    }
+
+    export module useraccount
+    {
+        
+    }
+
+    export module voltage
+    {
+        
+    }
+
+    export module volume
+    {
+        
+    }
+
+    export module volumequotasetting
+    {
+        
+    }
+
+    export module volumeuserquota
+    {
+        
+    }
+
+    export module wmiset
+    {
+        
+    }
+
 }
 export default wmic;
